1a791c1d790e27b0ee2686356e123612fb922263
efode <efidesalew@gmail.com>
2019-11-25 09:20:46 -0500

    Updating Fingerprint Generator

--- Core/FingerprintGenerator.py
+++ Core/FingerprintGenerator.py
@@ -15,3 +15,41 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from datasketch import MinHash
+
+
+class FingerprintGenerator(object):
+    def __init__(self, min_t_delta=0, max_t_delta=100, fan_out=5):
+        """
+
+        :param min_t_delta:
+        :param max_t_delta:
+        :param fan_out:
+        """
+        # min_t_delta, max_t_delta and min_t_delta defines the size of the target zone
+        self.min_t_delta = min_t_delta
+        self.max_t_delta = max_t_delta
+        self.fan_out = fan_out
+
+    def generate_fingerprints(self, spectral_peaks, audio_hashes, audio_hashes_info):
+        """
+
+        :param spectral_peaks:
+        :param audio_hashes:
+        :param audio_hashes_info:
+        """
+        for i in range(len(spectral_peaks)):
+            for j in range(1, self.fan_out):
+                if (i + j) < len(spectral_peaks):
+                    f1 = spectral_peaks[i][0]
+                    f2 = spectral_peaks[i + j][0]
+                    t1 = spectral_peaks[i][1]
+                    t2 = spectral_peaks[i + j][1]
+                    t_delta = t2 - t1
+                    if self.min_t_delta <= t_delta <= self.max_t_delta:
+                        peak_info = {str(f1), str(f2), str(t_delta)}
+                        peak_digest = MinHash(num_perm=10)
+                        for d in peak_info:
+                            peak_digest.update(d.encode('utf8'))
+                        audio_hashes.append(peak_digest)
+                        audio_hashes_info.append(t1)
