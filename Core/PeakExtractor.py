dfcf62aeff88389a622b173b0a4432b7d695c4cf
efode <efidesalew@gmail.com>
2019-11-25 09:11:14 -0500

    Updating PeakExtractor

--- Core/PeakExtractor.py
+++ Core/PeakExtractor.py
@@ -15,3 +15,87 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+import numpy as np
+from operator import itemgetter
+from skimage.feature import peak_local_max
+from scipy.ndimage import maximum_filter
+from scipy.ndimage import minimum_filter
+
+
+class PeakExtractor(object):
+    def __init__(self, filter_dim=10, minimum_amp_in_db=-60, num_peaks=60
+                 , maximum_filter_height=151, minimum_filter_height=3,
+                 maximum_filter_width=75, minimum_filter_width=3):
+        """
+
+        :param filter_dim:
+        :param minimum_amp_in_db:
+        :param num_peaks:
+        :param maximum_filter_height:
+        :param minimum_filter_height:
+        :param maximum_filter_width:
+        :param minimum_filter_width:
+        """
+        self.filter_dim = filter_dim
+        self.minimum_amp_in_db = minimum_amp_in_db
+        self.num_peaks = num_peaks
+        self.spectral_peaks = None
+        self.time_indices = None
+        self.frequency_indices = None
+        self.maximum_filter_height = maximum_filter_height
+        self.maximum_filter_width = maximum_filter_width
+        self.minimum_filter_height = minimum_filter_height
+        self.minimum_filter_width = minimum_filter_width
+
+    def extract_spectral_peaks_1(self, spectrogram_magnitude_in_db, fixed=False):
+        if fixed:
+            local_maxima = peak_local_max(image=spectrogram_magnitude_in_db,
+                                          min_distance=self.filter_dim,
+                                          indices=False,
+                                          threshold_abs=self.minimum_amp_in_db, num_peaks=self.num_peaks)
+        else:
+            local_maxima = peak_local_max(image=spectrogram_magnitude_in_db,
+                                          min_distance=self.filter_dim,
+                                          indices=False,
+                                          threshold_abs=self.minimum_amp_in_db)
+        self.frequency_indices, self.time_indices = np.where(local_maxima)
+        self.spectral_peaks = list(zip(self.frequency_indices, self.time_indices))
+        self.spectral_peaks.sort(key=itemgetter(1))
+
+    def extract_spectral_peaks_2(self, spectrogram):
+        """
+
+        :param spectrogram:
+        :return:
+        """
+        # computing local maximum points with the specified maximum filter dimension
+        local_max_values = maximum_filter(input=spectrogram, size=(self.maximum_filter_height,
+                                                                   self.maximum_filter_width))
+        # extracting time and frequency information for local maximum points
+        j, i = np.where(spectrogram == local_max_values)
+        peaks = list(zip(i, j))
+        # computing local minimum points with specified minimum filter dimension
+        local_min_values = minimum_filter(input=spectrogram, size=(self.minimum_filter_height,
+                                                                   self.minimum_filter_width))
+        # extracting time and frequency information for local minimums
+        k, m = np.where(spectrogram == local_min_values)
+        lows = list(zip(m, k))
+        # avoiding spectral points with are both local maximum and local minimum
+        spectral_peaks = list(set(peaks) - set(lows))
+        # time and frequency information for extracted spectral peaks
+        time_indices = [i[0] for i in spectral_peaks]
+        freq_indices = [i[1] for i in spectral_peaks]
+        spectral_peaks.sort(key=itemgetter(0))
+        return spectral_peaks, time_indices, freq_indices
+
+    def get_spectral_peaks(self):
+        return self.spectral_peaks
+
+    def get_time_indices(self):
+        return self.time_indices
+
+    def get_frequency_indices(self):
+        return self.frequency_indices
+
+    def get_time_frequency_indices(self):
+        return self.time_indices, self.frequency_indices
