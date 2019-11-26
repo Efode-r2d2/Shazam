4532a7a86395391ede3aada36c26dd3f3f49c0b9
efode <efidesalew@gmail.com>
2019-11-26 08:39:57 -0500

    Updating Fingerprint Matching

--- Matching/MatchFingerprints.py
+++ Matching/MatchFingerprints.py
@@ -15,3 +15,26 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from collections import defaultdict
+from FingerprintManager import FingerprintManager
+
+
+def match_fingerprints(self, fingerprint_file, audio_fingerprints, audio_fingerprints_info):
+    """
+
+    :param self:
+    :param fingerprint_file:
+    :param audio_fingerprints:
+    :param audio_fingerprints_info:
+    :return:
+    """
+    matches_in_bins = defaultdict(list)
+    query_seq = 0
+    for query in audio_fingerprints:
+        matches = FingerprintManager.query_fingerprint(lsh=fingerprint_file, audio_fingerprint=query)
+        for i in matches:
+            fingerprint_info = i.split("_")
+            # keep in mind the difference between hash_info and hashes_info
+            matches_in_bins[fingerprint_info[0]].append(int(fingerprint_info[2]) - audio_fingerprints_info[query_seq])
+        query_seq += 1
+    return matches_in_bins
