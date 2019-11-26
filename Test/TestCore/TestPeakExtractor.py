ca887000ebbe7c0bb0ed61625b7d8ff96bdb98ef
efode <efidesalew@gmail.com>
2019-11-26 06:44:50 -0500

    Updating Test Class for Peak Extractor

---
+++ Test/TestCore/TestPeakExtractor.py
@@ -0,0 +1,27 @@
+"""
+    < This program reproduced a work published in https://www.ee.columbia.edu/~dpwe/papers/Wang03-shazam.pdf>
+    Copyright (C) <2019>  <Efriem Desalew Gebie>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from Utilities import AudioManager
+from Utilities import DirManager
+from Utilities import GraphManager
+from Core import Spectrogram
+from Core import PeakExtractor
+
+# source directory
+src_dir = "../../../Test_Data/Reference_Audios"
+# searching for all .mp3 files under given source directory
+mp3_files = DirManager.find_mp3_files(src_dir=src_dir)
